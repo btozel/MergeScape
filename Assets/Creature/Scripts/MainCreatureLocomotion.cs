8f96be920da02ee94a0e57a4bd4a7f7e6f2d4a63
Serkan Ozel <299217+sozel@users.noreply.github.com>
2024-08-17 23:48:40 -0700

    main_creature

---
+++ Assets/Creature/Scripts/MainCreatureLocomotion.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class MainCreatureLocomotion : MonoBehaviour
+{
+
+    public float speed = 1.25f;
+
+	void Update () {
+		Vector3 pos = transform.position;
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			pos.z += speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			pos.z -= speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			pos.x += speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			pos.x -= speed * Time.deltaTime;
+		}
+			
+		transform.position = pos;
+	}
+}
